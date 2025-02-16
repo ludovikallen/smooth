f139958559f77c28a6b0022990ea4f77a4b640e9
ludovikallen <ludovikallen@gmail.com>
2025-02-16 15:14:01 -0500

    Add form for basic stack information (#5)

--- source/commands/createStack.tsx
+++ source/commands/createStack.tsx
@@ -1,13 +1,31 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {Box, Text, useInput, useApp} from 'ink';
 import TextInput from 'ink-text-input';
 import util from 'node:util';
 import child_process from 'node:child_process';
+import Divider from 'ink-divider';
 
 const execFile = util.promisify(child_process.execFile);
 
+enum Step {
+	TargetBookmark = 0,
+	StackName = 1,
+	BookmarkPrefix = 2,
+	CommitPrefix = 3,
+	EditingBlocks = 4,
+}
+
 const CreateStack: React.FC = () => {
+	const [currentStep, setCurrentStep] = useState<Step>(Step.TargetBookmark);
+	const [nextStep, setNextStep] = useState<Step | undefined>(
+		Step.BookmarkPrefix,
+	);
+	const [previousStep, setPreviousStep] = useState<Step | undefined>();
 	const [items, setItems] = useState<string[]>([]);
+	const [targetBookmark, setTargetBookmark] = useState<string>('');
+	const [bookmarkPrefix, setBookmarkPrefix] = useState<string>('');
+	const [commitPrefix, setCommitPrefix] = useState<string>('');
+	const [stackName, setStackName] = useState<string>('');
 	const [currentInput, setCurrentInput] = useState('');
 	const [selectedIndex, setSelectedIndex] = useState<number>(0);
 	const [isFirstItem, setIsFirstItem] = useState(true);
@@ -84,96 +102,223 @@ const CreateStack: React.FC = () => {
 		}
 	};
 
-	useInput((input: string, key: {upArrow: boolean; downArrow: boolean}) => {
-		if (!isFirstItem && !isAdding && !isEditing) {
-			if (key.upArrow) {
-				setSelectedIndex(Math.max(0, selectedIndex - 1));
-			}
-			if (key.downArrow) {
-				setSelectedIndex(Math.min(items.length - 1, selectedIndex + 1));
-			}
-			if (input === 'a') {
-				const newItems = [...items];
-				const newLength = items.length;
-				newItems.splice(newLength, 0, '');
-				setSelectedIndex(newLength);
-				setItems(newItems);
-				setIsEditing(true);
-			}
-			if (input === 'e') {
-				setCurrentInput(items[selectedIndex]!);
-				setIsEditing(true);
-			}
-			if (input === 'c') {
-				handleCompletion();
-			}
+	useEffect(() => {
+		const stepValues = Object.values(Step).filter(
+			value => typeof value === 'number',
+		) as Step[];
 
-			if (input === 'd') {
-				const newItems = [...items];
-				newItems.splice(selectedIndex, 1);
-				setItems(newItems);
+		const currentIndex = stepValues.indexOf(currentStep);
 
-				if (newItems.length === 0) {
-					setIsFirstItem(true);
-				} else {
-					setSelectedIndex(selectedIndex == 0 ? 0 : selectedIndex - 1);
+		const previousIndex =
+			currentIndex > 0 ? stepValues[currentIndex - 1] : undefined;
+
+		setPreviousStep(previousIndex);
+		const nextIndex =
+			currentIndex < stepValues.length - 1
+				? stepValues[currentIndex + 1]
+				: undefined;
+		setNextStep(nextIndex);
+	}, [currentStep]);
+
+	useInput(
+		(
+			input: string,
+			key: {
+				upArrow: boolean;
+				downArrow: boolean;
+				return: boolean;
+				tab: boolean;
+			},
+		) => {
+			if (
+				!isFirstItem &&
+				!isAdding &&
+				!isEditing &&
+				currentStep == Step.EditingBlocks
+			) {
+				if (key.upArrow) {
+					setSelectedIndex(Math.max(0, selectedIndex - 1));
+				}
+				if (key.downArrow) {
+					setSelectedIndex(Math.min(items.length - 1, selectedIndex + 1));
+				}
+				if (input === 'a') {
+					const newItems = [...items];
+					const newLength = items.length;
+					newItems.splice(newLength, 0, '');
+					setSelectedIndex(newLength);
+					setItems(newItems);
+					setIsEditing(true);
+				}
+				if (input === 'e') {
+					setCurrentInput(items[selectedIndex]!);
+					setIsEditing(true);
+				}
+				if (input === 'c') {
+					handleCompletion();
+				}
+
+				if (input === 'd') {
+					const newItems = [...items];
+					newItems.splice(selectedIndex, 1);
+					setItems(newItems);
+
+					if (newItems.length === 0) {
+						setIsFirstItem(true);
+					} else {
+						setSelectedIndex(selectedIndex == 0 ? 0 : selectedIndex - 1);
+					}
+				}
+			} else if (isEditing && currentStep == Step.EditingBlocks) {
+				if (key.upArrow) {
+					handleMoveUp();
+				}
+				if (key.downArrow) {
+					handleMoveDown();
+				}
+			} else {
+				if (key.return && nextStep != undefined) {
+					setCurrentStep(nextStep);
+				} else if (key.tab && previousStep != undefined) {
+					setCurrentStep(previousStep);
 				}
 			}
-		} else if (isEditing) {
-			if (key.upArrow) {
-				handleMoveUp();
-			}
-			if (key.downArrow) {
-				handleMoveDown();
-			}
-		}
-	});
+		},
+	);
 
-	return (
-		<>
-			{!isFirstItem && (
+	switch (currentStep) {
+		case Step.TargetBookmark: {
+			return (
 				<>
-					{items.map((item, index) => (
-						<Box key={index}>
-							{index === selectedIndex && isEditing ? (
-								<Box>
-									<Text color="yellow">
-										{'>'} {index}.{' '}
-									</Text>
-									<TextInput
-										value={currentInput}
-										onChange={setCurrentInput}
-										onSubmit={handleSubmit}
-									/>
+					<Box>
+						<Text>Target bookmark: </Text>
+						<TextInput value={targetBookmark} onChange={setTargetBookmark} />
+					</Box>
+					<ShortcutsMenu nextStep={nextStep} previousStep={previousStep} />
+				</>
+			);
+		}
+
+		case Step.StackName: {
+			return (
+				<>
+					<Box flexDirection="row">
+						<Text>Target bookmark: {targetBookmark}</Text>
+					</Box>
+
+					<Box>
+						<Text>Stack name: </Text>
+						<TextInput value={stackName} onChange={setStackName} />
+					</Box>
+					<ShortcutsMenu nextStep={nextStep} previousStep={previousStep} />
+				</>
+			);
+		}
+
+		case Step.BookmarkPrefix: {
+			return (
+				<>
+					<Box flexDirection="column">
+						<Text>Target bookmark: {targetBookmark}</Text>
+						<Text>Stack name: {stackName}</Text>
+					</Box>
+
+					<Box>
+						<Text>Bookmark prefix: </Text>
+						<TextInput value={bookmarkPrefix} onChange={setBookmarkPrefix} />
+					</Box>
+					<ShortcutsMenu nextStep={nextStep} previousStep={previousStep} />
+				</>
+			);
+		}
+
+		case Step.CommitPrefix: {
+			return (
+				<>
+					<Box flexDirection="column">
+						<Text>Target bookmark: {targetBookmark}</Text>
+						<Text>Stack name: {stackName}</Text>
+						<Text>Bookmark prefix: {bookmarkPrefix}</Text>
+					</Box>
+
+					<Box>
+						<Text>Commit prefix: </Text>
+						<TextInput value={commitPrefix} onChange={setCommitPrefix} />
+					</Box>
+					<ShortcutsMenu nextStep={nextStep} previousStep={previousStep} />
+				</>
+			);
+		}
+
+		case Step.EditingBlocks: {
+			return (
+				<>
+					<Box flexDirection="column">
+						<Text>Target bookmark: {targetBookmark}</Text>
+						<Text>Stack name: {stackName}</Text>
+						<Text>Bookmark prefix: {bookmarkPrefix}</Text>
+						<Text>Commit prefix: {commitPrefix}</Text>
+					</Box>
+					<Divider />
+					{!isFirstItem && (
+						<>
+							{items.map((item, index) => (
+								<Box key={index}>
+									{index === selectedIndex && isEditing ? (
+										<Box>
+											<Text color="yellow">
+												{'>'} {index}.{' '}
+											</Text>
+											<TextInput
+												value={currentInput}
+												onChange={setCurrentInput}
+												onSubmit={handleSubmit}
+											/>
+										</Box>
+									) : (
+										<Text>
+											{index === selectedIndex ? '>' : ' '} {index}. {item}
+										</Text>
+									)}
 								</Box>
+							))}
+							{isEditing ? (
+								<Text>Save (enter) | Move (↑↓)</Text>
 							) : (
 								<Text>
-									{index === selectedIndex ? '>' : ' '} {index}. {item}
+									Navigate (↑↓) | Add (a) | Edit (e) | Delete (d) | Complete (c)
 								</Text>
 							)}
-						</Box>
-					))}
-					{isEditing ? (
-						<Text>Save (enter) | Move (↑↓)</Text>
-					) : (
-						<Text>
-							Navigate (↑↓) | Add (a) | Edit (e) | Delete (d) | Complete (c)
-						</Text>
+						</>
+					)}
+					{isFirstItem && (
+						<>
+							<Text>What should the first block be named?</Text>
+							<TextInput
+								value={currentInput}
+								onChange={setCurrentInput}
+								onSubmit={handleSubmit}
+							/>
+						</>
 					)}
 				</>
-			)}
-			{isFirstItem && (
-				<>
-					<Text>What should the first block be named?</Text>
-					<TextInput
-						value={currentInput}
-						onChange={setCurrentInput}
-						onSubmit={handleSubmit}
-					/>
-				</>
-			)}
-		</>
-	);
+			);
+		}
+	}
 };
 
+interface ShortcutsMenuProps {
+	nextStep: Step | undefined;
+	previousStep: Step | undefined;
+}
+
+const ShortcutsMenu = ({nextStep, previousStep}: ShortcutsMenuProps) => {
+	if (nextStep !== undefined && previousStep !== undefined) {
+		return <Text>↵ (next) | ↹ (previous)</Text>;
+	} else if (nextStep !== undefined && previousStep == undefined) {
+		return <Text>↵ (next)</Text>;
+	} else {
+		return <Text>↹ (previous)</Text>;
+	}
+};
 export default CreateStack;
