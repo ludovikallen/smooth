d3bccadc71a40b01c9c0d63b9fa453de2efa59a3
ludovikallen <ludovikallen@gmail.com>
2025-02-20 21:07:39 -0500

    Add options to add and remove blocks within current command (#16)

--- source/repository.ts
+++ source/repository.ts
@@ -24,12 +24,47 @@ export async function findAllBlocksByStackIdOrderedByIndex(
 		.execute();
 }
 
+export async function insertBlockAtIndex(block: NewBlock, stackId: number) {
+	const blocks = await db
+		.selectFrom('block')
+		.where('block.stack_id', '=', stackId)
+		.where('block.index', '>=', block.index)
+		.selectAll()
+		.execute();
+
+	for (const currentBlock of blocks) {
+		currentBlock.index++;
+		await db
+			.updateTable('block')
+			.set(currentBlock)
+			.where('id', '=', currentBlock.id)
+			.execute();
+	}
+
+	await db
+		.insertInto('block')
+		.values({
+			name: block.name,
+			index: block.index,
+			change_id: block.change_id,
+			bookmark_name: block.bookmark_name,
+			is_submitted: block.is_submitted,
+			is_done: block.is_done,
+			stack_id: stackId,
+		})
+		.execute();
+}
+
 export async function updateBlock(id: number, block: BlockUpdate) {
 	return await db
 		.updateTable('block')
 		.set(block)
 		.where('id', '=', id)
 		.execute();
+}
+
+export async function deleteBlock(id: number) {
+	return await db.deleteFrom('block').where('id', '=', id).execute();
 }
 
 export async function createStack(stack: NewStack, blocks: NewBlock[]) {
